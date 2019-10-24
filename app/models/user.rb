112dc34a4a9ae18baa1db3c42dff12c523be22c0
Cristian Garcia <eminemko@hotmail.com>
2019-10-24 10:28:42 -0500

    Add message model and table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,8 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness:true, length: {minimum: 3, maximum: 15}
+  validates :username,
+              presence: true,
+              uniqueness: {case_sensitive: false},
+              length: {minimum: 3, maximum: 15}
+  has_many :messages
   has_secure_password
 end
-
-
-User.create(username: "Cristian", password: "password")
-User.create(username: "Fernando", password: "password")
-User.create(username: "Jesus", password: "password")
-User.create(username: "David", password: "password")
-User.create(username: "Oscar", password: "password")
